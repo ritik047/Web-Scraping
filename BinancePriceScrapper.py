8101a13b6c8ac4a9ba323804b62a322e46f5c09f
Ritik Gulabrani <52580488+ritik047@users.noreply.github.com>
2022-10-15 01:27:54 +0530

    Update BinancePriceScrapper.py

--- BinancePriceScrapper.py
+++ BinancePriceScrapper.py
@@ -4,8 +4,11 @@ BINANCE_ENDPOINT="https://www.binance.com/bapi/composite/v1/public/marketing/sym
 
 response = requests.get(BINANCE_ENDPOINT)
 
+i=0
 if response.status_code==200:
     for asset in json.loads(response.text).get("data"):
+        print(i,end=")")
+        i=i+1
         print("NAME: {}\nSYMBOL: {}\nPRICE: {}\nTOTAL-SUPPLY: {}\n24HRS-PRICE-CHANGE: {}\n24HRS-PERCENT-CHANGE: {}"
               .format(asset.get("name"), asset.get("symbol"), asset.get("price"), asset.get("totalSupply"), asset.get("dayChange"), asset.get("dayChangeAmount")), end="\n"+"="*100+"\n")
 
